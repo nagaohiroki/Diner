94ef30da5b08771f5b8270e2692890de1c07cdc6
nagaohiroki <testplayers0226@gmail.com>
2023-01-24 17:05:23 +0900

    パスワード周り修正

--- UnityProject/Assets/Scripts/MenuJoin.cs
+++ UnityProject/Assets/Scripts/MenuJoin.cs
@@ -5,4 +5,9 @@ public class MenuJoin : MonoBehaviour
 	[SerializeField]
 	TMP_InputField mPassword;
 	public string GetPassword => mPassword.text;
+	public void OnValueChanged()
+	{
+		var text = mPassword.text;
+		mPassword.text = text.ToUpper();
+	}
 }
--- UnityProject/Assets/Scripts/MenuLoading.cs
+++ UnityProject/Assets/Scripts/MenuLoading.cs
@@ -1,4 +1,20 @@
 ﻿using UnityEngine;
 public class MenuLoading : MonoBehaviour
 {
+	[SerializeField]
+	RectTransform mTrans;
+	float mPower = 1.0f;
+	float mSpeed = 1.0f;
+	float mAccel;
+	void Start()
+	{
+		mAccel = 0.0f;
+	}
+	void Update()
+	{
+		mAccel += Time.deltaTime * mSpeed;
+		var euler = mTrans.eulerAngles;
+		euler.z += mPower * mAccel;
+		mTrans.eulerAngles = euler;
+	}
 }
