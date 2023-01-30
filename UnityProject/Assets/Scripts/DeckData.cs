c7e1475594d3c8faf0551ffc47b9cf23292f597e
nagaohiroki <testplayers0226@gmail.com>
2023-01-30 18:43:47 +0900

    add cook card

---
+++ UnityProject/Assets/Scripts/DeckData.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class CardCounter
+{
+#if UNITY_EDITOR
+	[SerializeField]
+	string id;
+#endif
+	[SerializeField]
+	CardData mCard;
+	[SerializeField]
+	int mNum;
+	public CardData GetCard => mCard;
+	public int GetNum => mNum;
+	public override string ToString()
+	{
+		return $"{mCard} x{mNum}";
+	}
+#if UNITY_EDITOR
+	public void OnValidate()
+	{
+		id = ToString();
+	}
+#endif
+}
+[CreateAssetMenu]
+public class DeckData : ScriptableObject
+{
+	[SerializeField]
+	string id;
+	[SerializeField]
+	int supply;
+	[SerializeField]
+	List<CardCounter> mCard;
+	public string GetId => id;
+	public int GetSupply => supply;
+	public List<CardData> GenerateCardList()
+	{
+		var list = new List<CardData>();
+		foreach(var card in mCard)
+		{
+			for(int i = 0; i < card.GetNum; i++)
+			{
+				list.Add(card.GetCard);
+			}
+		}
+		return list;
+	}
+	public override string ToString()
+	{
+		var log = $"{id}:\n";
+		int total = 0;
+		foreach(var card in mCard)
+		{
+			log += $"{card}\n";
+			total += card.GetNum;
+		}
+		log += $"---\nTotal:{total}\n";
+		return log;
+	}
+#if UNITY_EDITOR
+	public void OnValidate()
+	{
+		foreach(var card in mCard)
+		{
+			card.OnValidate();
+		}
+	}
+#endif
+}
