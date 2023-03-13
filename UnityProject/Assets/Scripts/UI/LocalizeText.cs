6dcabb666f0b81550d1c7df11ec2afbe17257f5f
nagaohiroki <testplayers0226@gmail.com>
2023-03-13 12:49:33 +0900

    UI準備 #5

---
+++ UnityProject/Assets/Scripts/UI/LocalizeText.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class TextData
+{
+	[SerializeField]
+	string id;
+}
+[CreateAssetMenu]
+public class LocalizeText : ScriptableObject
+{
+}
---
+++ UnityProject/Assets/Scripts/UI/UIText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+public class UIText : MonoBehaviour
+{
+	[SerializeField]
+	string mTextId;
+	[SerializeField]
+	TextMeshProUGUI mText;
+#if UNITY_EDITOR
+	void OnValidate()
+	{
+		if(mText == null)
+		{
+			TryGetComponent<TextMeshProUGUI>(out mText);
+		}
+	}
+#endif
+}
